8e056d91d307181ce5ab43ce9d50f2715fda2365
Abhimanyu Bathla <bathlaabhimanyu14@gmail.com>
2025-09-07 18:50:39 +0530

    Update q1.cpp

--- Assignment 3/q1.cpp
+++ Assignment 3/q1.cpp
@@ -1,22 +1,85 @@
-#include <iostream>
+#include<iostream>
+#include <string>
 using namespace std;
-#define MAX 100
-int stack[MAX], top=-1;
-void push(int x){if(top<MAX-1)stack[++top]=x;}
-int pop(){if(top>=0)return stack[top--];return -1;}
-int peek(){if(top>=0)return stack[top];return -1;}
-int isEmpty(){return top==-1;}
-int isFull(){return top==MAX-1;}
-void display(){for(int i=top;i>=0;i--)cout<<stack[i]<<" ";}
-int main(){
-    int ch,val;
-    do{
-        cout<<"1.Push 2.Pop 3.Peek 4.Display 5.Exit
-";
-        cin>>ch;
-        if(ch==1){cin>>val;push(val);}
-        else if(ch==2)cout<<pop()<<endl;
-        else if(ch==3)cout<<peek()<<endl;
-        else if(ch==4)display();
-    }while(ch!=5);
+void push(int stack[],int &top,int maxstack,int item){
+    if(top==maxstack){
+        cout<<"The stack is full"<<endl;
+        return;
+    }
+    top=top+1;
+    cout<<"enter the number to be added "<<endl;
+    cin>>item;
+    stack[top]=item;
 }
+int pop(int stack[],int &top,int maxstack,int item){
+    if(top==-1){
+        cout<<"The stack is empty"<<endl;
+        return -1;
+    }
+    item=stack[top];
+    top=top-1;
+    return item;
+}
+void display(int stack[], int top) {
+    if (top == -1) {
+        cout << "Stack is empty." << endl;
+        return;
+    }
+    cout << "Stack contents: ";
+    for (int i = 0; i <= top; i++) {
+        cout << stack[i] << " ";
+    }
+    cout << endl;
+}
+void peek(int stack[],int &top,int maxstack,int item){
+    cout<<"The top is:"<<stack[top];
+}
+int main(){
+     int maxstack;
+    cout << "Enter maximum stack size: ";
+    cin >> maxstack;
+
+    int *stack = new int[maxstack];
+    int top = -1;
+
+    int choice, item;
+
+    do {
+        cout << "\n--- Stack Menu ---\n";
+        cout << "1. Push\n2. Pop\n3. Display\n4.peek\n5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                push(stack, top, maxstack, item);
+                break;
+
+            case 2:
+                item = pop(stack, top,maxstack,item);
+                if (item != -1)
+                    cout << "Popped item: " << item << endl;
+                break;
+
+            case 3:
+                display(stack, top);
+                break;
+
+            case 4:
+            peek( stack, top, maxstack, item);
+            break;
+
+            case 5:
+                cout << "Exiting..." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice, try again." << endl;
+        }
+
+    } while (choice != 5);
+
+    delete[] stack;  
+    return 0;
+}
+
